c1b17f68f3c1f51678d65f5cc114addb7eb7290c
Cloud <m.melanson000@gmail.com>
2022-09-07 11:59:14 -0400

    added category model

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./user');
+const Review = require('./review');
+const Category = require('./category');
+const Listing = require('./listing');
+
+module.exports = {
+  User,
+  Review,
+  Category,
+  Listing,
+};

edcc4d59420d24e5efed65df72bb3cf5f1c01419
Cloud <m.melanson000@gmail.com>
2022-09-08 19:20:15 -0400

    fixed type

--- seeds/index.js
+++ seeds/index.js
@@ -8,17 +8,18 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedReview();
-  console.log('\n----- REVIEWS SEEDED -----\n');
-
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
 
   await seedUser();
   console.log('\n----- USER SEEDED -----\n');
 
+  await seedCategories();
+  console.log('\n----- CATEGORIES SEEDED -----\n');
+
   await seedListing();
   console.log('\n----- LISTING SEEDED -----\n');
+
+  await seedReview();
+  console.log('\n----- REVIEWS SEEDED -----\n');
 
   process.exit(0);
 };

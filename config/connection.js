41622966bd6e093a1d9301ab8857f069ef9e2964
Cloud <m.melanson000@gmail.com>
2022-09-08 21:41:02 -0400

    server boots up again, yay!

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,15 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'mysql',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
-
-const sessionConfig = {
-  secret: process.env.SECRET,
-  cookie: {
-    maxAge: 1 * 60 * 60 * 1000, // 1 * 60 * 60 * 1000 === 1 hour
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-module.exports = { sequelize, sessionConfig };
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -2,19 +2,41 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helper.js');
-const { sequelize, sessionConfig } = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = process.env.PORT || 3001;
+const routes = require('./controllers');
+const helper = require('./utils/helper.js');
+const sequelize = require('./config/connection');
+
+require('dotenv').config();
+
 const app = express();
-const hbs = exphbs.create({ helpers });
+const PORT = process.env.PORT || 3001;
+
+const sessionConfig = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 1 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 app.use(session(sessionConfig));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+const hbs = exphbs.create({ helper });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);

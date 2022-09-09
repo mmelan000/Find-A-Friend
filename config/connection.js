5c49f8615a61cf459dcbb7d0e9dd60da17a87f0c
Jovy Ira <ira_bonching@yahoo.com>
2022-09-09 02:02:42 -0400

    fixed some errors in sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,34 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
+console.log(process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD)
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(
+//       process.env.DB_NAME,
+//       process.env.DB_USER,
+//       process.env.DB_PASSWORD,
+//       {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//       }
+//     );
+
+    const sequelize = new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
         host: 'localhost',
         dialect: 'mysql',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
+        port: 3306
       }
     );
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const viewRoutes = require('./views');
 
 router.use('/api', apiRoutes);
+router.use('/views', viewRoutes);
 
 module.exports = router;

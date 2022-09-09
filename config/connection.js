8590f997461d1b3a53a2b7abeda25ba53c278d6f
Jovy Ira <ira_bonching@yahoo.com>
2022-09-09 02:10:58 -0400

    revert sequelize file

--- config/connection.js
+++ config/connection.js
@@ -4,24 +4,9 @@ const Sequelize = require('sequelize');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-console.log(process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD)
-
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(
-//       process.env.DB_NAME,
-//       process.env.DB_USER,
-//       process.env.DB_PASSWORD,
-//       {
-//         host: 'localhost',
-//         dialect: 'mysql',
-//         port: 3306
-//       }
-//     );
-
-    const sequelize = new Sequelize(
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,

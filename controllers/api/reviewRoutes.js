00bd24df47e911905bd70da64325c6a0f5dc14b8
Zoot83 <marshallrizzuto@gmail.com>
2022-09-11 13:58:07 -0400

    delete route for review

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -57,6 +57,25 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedreview = await Review.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedreview) {
+      return res
+        .status(404)
+        .json({ message: 'No listing found with that id.' });
+    }
+    return res.status(200).json({
+      message: 'That listing has been deleted with that id',
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
 module.exports = router;
